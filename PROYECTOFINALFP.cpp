44dd687f0a0d4ff10692621887e764ef22b9c232
afloresh1 <afloresh@unjbg.edu.pe>
2024-08-09 02:13:58 -0500

    Se modificó para adaptarlo a .h

--- PROYECTOFINALFP.cpp
+++ PROYECTOFINALFP.cpp
@@ -3,82 +3,12 @@
 #include <conio.h>
 #include <string>
 #include <stdio.h>
-
+#include "menuinicial.h"
+#include"main.h"
 using namespace std;
-enum Inicio{
-	ejecutar,
-	salir,
-	finalizar
-};
+
 
 //FUNCIONES----
-
-void mostrarbienvenido(int kbhit());
-string funcioninicio(Inicio boton);
-
-//PROGRAMA PRINCIPAL
-
-int main(){
-	string input;
-	char s;
-	string boton;
-	string boton1;
-	int cmode=0;
-	
-	//FUNCION PARA LAS DOS PANTALLAS MOSTRADAS (Y SE PODRIA  SALIR DEL BUCLE CUANDO SE PRESIONE UNA TECLA//
-		mostrarbienvenido(kbhit);
-		
-		
-	//A PARTIR DE AQUI SE PONDRA LO QUE SALDRIA LUEGO DE PULSAR CUALQUIER TECLA PARA INICIAR//
-	//----------------------------------------------------------------------------------------------------
-	for(int i=1;i<=5;i++){
-	cout<<"              "<<endl;
-	}
-	cout<<"                                  ";
-	cout<<"INDICACIONES: "<<endl;
-	for(int i=1;i<=5;i++){
-	cout<<"              "<<endl;
-	}
-	cout<<"     ";
-	cout<<"Presione las letras - EJECUTAR - para dar inicio al programa"<<endl;
-	cout<<"     ";
-	cout<<"Presione las letras - SALIR - para dejar de ejecutar el programa"<<endl;
-	cout<<"     ";
-	cout<<"Presione las letras - FINALIZAR - para finalizar el programa"<<endl;
-	cout<<" "<<endl;
-	cout<<"     ";
-	//---------------------------------------------------------------------------------------------------
-	cout<<"Ingrese la palabra: ";
-	cin>>boton;
-	if(boton=="ejecutar"){
-		cout<< funcioninicio(ejecutar);
-	}else if(boton =="salir"){
-		cout<< funcioninicio(salir);
-	}else if (boton=="finalizar"){
-		cout<< funcioninicio(finalizar);
-	}else{
-		system("cls");
-		//---------------------------------------------------
-	    for(int i=1;i<=5;i++){
-          	cout<<"              "<<endl;
-          	cout<<" "<<endl;
-     	}
-    	cout<<"                            ";
-		cout<<"Palabra no reconocida";
-        cout<<"                            ";
-        
-		for(int i=1;i<=5;i++){
-          	cout<<"              "<<endl;
-          	cout<<" "<<endl;
-		}
-		//----------------------------------------------------
-		system("pause");
-		system("cls");
-        return main();
-		}
-		
-	
-}
 
 
 
